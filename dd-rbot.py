a0fddc321a8d85a7f7dad54cbb8ffa3557495ca4
InnocentCow <julesstolk@gmail.com>
2022-09-18 01:02:30 +0200

    changes on le macbook

--- dd-rbot.py
+++ dd-rbot.py
@@ -6,16 +6,26 @@ from discord.ext import commands
 
 bot = discord.Bot()
 bot = commands.Bot(command_prefix="=")
-boardmessage = ""
 playerselectoptions = [discord.SelectOption(label="Don't press please :D", description="Placeholder else the code'll break")]
+game = {}
+samplegamesave = {
+    "hp": 20, 
+    "board": [], 
+    "hand": [], 
+    "deck": [], 
+    "magic": 0, 
+    "maxmagic": 0, 
+    "grave": [], 
+    "banish": []
+}
 
 def load(channel):
     with open(f"DDMatches/{str(channel)}.json") as f:
         return json.load(f)
 
-def save(channel, gamesav):
+def save(channel, game):
     with open(f"DDMatches/{channel}.json", "w") as f:
-        json.dump(gamesav, f)
+        json.dump(game, f)
 
 def mktemp(player, P1, P2):
     if player == P1:
@@ -29,7 +39,7 @@ def makeBoardASCII(channel):
     endrow = ""
     for i in range(1, 3):
         player = "P" + str(i)
-        row = f"| HP {player}:{game['HP' + player]} | Hand:{len(game['hand' + player])} | Deck:{len(game['deck' + player])} | Magic:{game['magic' + player]} | Max Magic:{game['maxmagic' + player]} | Grave:{len(game['grave' + player])} | Banished:{len(game['banish' + player])} |"
+        row = f"| HP {player}:{game['hp' + player]} | Hand:{len(game['hand' + player])} | Deck:{len(game['deck' + player])} | Magic:{game['magic' + player]} | Max Magic:{game['maxmagic' + player]} | Grave:{len(game['grave' + player])} | Banished:{len(game['banish' + player])} |"
         endrow += row + "\n"
         if i == 1:
             for j in range(1, 3):
@@ -58,10 +68,21 @@ def makeBoardASCII(channel):
 #             ]
 
 class buttonBoard(discord.ui.View):
-    global boardmessage
     @discord.ui.button(label="See", style=discord.ButtonStyle.primary)
+
     async def seesomething(self, button, interaction):
         await interaction.response.edit_message(view = seeBoard())
+
+    @discord.ui.button(label="Play", style=discord.ButtonStyle.primary)
+
+    async def play_callback:
+        await interaction.response.edit_message(view = playCard())
+
+    @discord.ui.button(label="Draw", style=discord.ButtonStyle.primary)
+
+    async def draw_callback:
+        game{}
+
 
 class seeBoard(discord.ui.View):
     @discord.ui.button(label="Hand", style=discord.ButtonStyle.primary)
@@ -78,27 +99,9 @@ class seeBoard(discord.ui.View):
         temp = mktemp(str(interaction.user)[0:-5].lower(), P1, P2)
         await interaction.response.send_message(game['deck' + temp], ephemeral = True)
 
-# class choosePlayers(discord.ui.View, playerselectoptions):
-#     playerselectoptions = [discord.SelectOption(label="Don't press please :D", description="Placeholder else the code'll break")]
-#     for item in openmatches:
-#         playerselectoptions.append(discord.SelectOption(label=item))
-    
-#     @discord.ui.select(placeholder="Choose player", min_values=1, max_values=1, options = optionsselect())
-
-#     async def select_callback(self, select, interaction):
-#         playerselectoptions.remove(select.values[0])
-#         newChannel = await interaction.create_text_channel(str(interaction.user)[0:-5] + str(select.values[0]), position = 5)
-#         await interaction.response.edit_message(f"Created channel {newChannel}")
-
-#     @discord.ui.button(label="Add myself", style=discord.ButtonStyle.primary)
-
-#     async def button_callback(self, button, interaction):
-#         openmatches.append(str(interaction.user)[0:-5])
-#         await interaction.response.edit_message(content="Added you to the open matches list", view=None)
 
 @bot.slash_command()
 async def start(ctx):
-    global boardmessage
     await ctx.respond(makeBoardASCII(ctx.channel), view = buttonBoard())
 
 
@@ -113,9 +116,17 @@ async def startmatch(ctx):
                 if item.label == select.values[0]:
                     playerselectoptions.remove(item)
                     matchplayer = str(item.label)
+                    otherplayer = str(interaction.user[0:-5])
                     break
-            newChannel = await interaction.guild.create_text_channel("`" + str(interaction.user)[0:-5] + "-" + matchplayer + "`", category=interaction.channel.category, position = 9)
+            newChannel = await interaction.guild.create_text_channel("`" + otherplayer + "-" + matchplayer + "`", category=interaction.channel.category, position = 9)
             await interaction.response.edit_message(content=f"Created channel {newChannel}")
+            with open(f"games\\{otherplayer}-{matchplayer}.json") as f:
+                newgamesave = {}
+                for key in samplegamesave:
+                    newgamesave[key + otherplayer] = samplegamesave[key]
+                    newgamesave[key + matchplayer] = samplegamesave[key]
+                json.dump(newgamesave, f)
+                
 
         @discord.ui.button(label="Add myself", style=discord.ButtonStyle.primary)
 
